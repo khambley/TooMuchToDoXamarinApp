ba982e606c83ff132f3a65dc682ab3509fb859ba
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 18:39:19 -0500

    Added ToDoItemRepository interface and implementation pg 52

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Repositories/ITodoItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TooMuchToDoXamarinApp.Models;
+
+namespace TooMuchToDoXamarinApp.Repositories
+{
+	public interface ITodoItemRepository
+	{
+		event EventHandler<TodoItem> OnItemAdded;
+		event EventHandler<TodoItem> OnItemUpdated;
+
+		Task<List<TodoItem>> GetItems();
+		Task AddItem(TodoItem item);
+		Task UpdateItem(TodoItem item);
+		Task AddOrUpdate(TodoItem item);
+
+		//TODO: Add Delete method pg.51
+	}
+}
---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Repositories/TodoItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TooMuchToDoXamarinApp.Models;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TooMuchToDoXamarinApp.Repositories
+{
+	public class TodoItemRepository : ITodoItemRepository
+	{
+		public event EventHandler<TodoItem> OnItemAdded;
+		public event EventHandler<TodoItem> OnItemUpdated;
+
+		public async Task<List<TodoItem>> GetItems()
+		{
+			return null; //Just to get it to build for now.
+		}
+
+		public async Task AddItem(TodoItem item)
+		{
+
+		}
+
+		public async Task UpdateItem(TodoItem item)
+		{
+		}
+
+		public async Task AddOrUpdate(TodoItem item)
+		{
+			if (item.Id == 0)
+			{
+				await AddItem(item);
+			}
+			else
+			{
+				await UpdateItem(item);
+			}
+		}
+
+
+	}
+}
