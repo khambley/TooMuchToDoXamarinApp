e2357dcd1dac24c6471e478606d9701edee8d3bd
Katherine Hambley <superdesigngirl@mac.com>
2021-09-06 17:27:43 -0500

    Added Save button functionality on ItemViewModel pg.78-79

--- TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/ItemViewModel.cs
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/ItemViewModel.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TooMuchToDoXamarinApp.Models;
 using TooMuchToDoXamarinApp.Repositories;
+using Xamarin.Forms;
 
 namespace TooMuchToDoXamarinApp.ViewModels
 {
@@ -11,9 +14,21 @@ namespace TooMuchToDoXamarinApp.ViewModels
 	{
 		private readonly TodoItemRepository repository;
 
+		public TodoItem Item { get; set; }
+
 		public ItemViewModel(TodoItemRepository repository)
 		{
 			this.repository = repository;
+			Item = new TodoItem() { DueDate = DateTime.Now.AddDays(1) };
 		}
+
+		public ICommand Save => new Command(async () =>
+		{
+			await repository.AddOrUpdate(Item);
+
+			//"Popping the stack" - removes the topmost item from the stack.
+			//"Pushing a page" - adds a page to the navigation stack - PushAsync()
+			await Navigation.PopAsync();
+		});
 	}
 }
