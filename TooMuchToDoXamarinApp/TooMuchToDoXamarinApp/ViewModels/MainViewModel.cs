f8d2e696ff2edbf2c34abc647f2deb3cc7330300
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 22:40:09 -0500

    Added the MainViewModel and the TodoItemViewModel classes pg.61-63

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TooMuchToDoXamarinApp.Repositories;
+
+namespace TooMuchToDoXamarinApp.ViewModels
+{
+	public class MainViewModel : ViewModel
+	{
+		private readonly TodoItemRepository repository;
+
+		public MainViewModel(TodoItemRepository repository)
+		{
+			this.repository = repository;
+			Task.Run(async () => await LoadData());
+		}
+
+		private async Task LoadData()
+		{
+
+		}
+	}
+}
---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TooMuchToDoXamarinApp.Models;
+
+namespace TooMuchToDoXamarinApp.ViewModels
+{
+	public class TodoItemViewModel : ViewModel
+	{
+		public TodoItemViewModel(TodoItem item)
+		{
+			Item = item;
+		}
+		public event EventHandler ItemStatusChanged;
+		public TodoItem Item { get; private set; }
+
+		public string StatusText => Item.Completed ? "Reactivate" : "Completed";
+	}
+}
