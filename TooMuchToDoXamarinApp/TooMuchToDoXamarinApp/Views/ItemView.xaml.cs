4121619bcbea98d5cb3b29aab8a3cba19abe6b7e
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 23:07:16 -0500

    Added ItemView and wired up ItemViewModel in code-behind pg 67

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Views/ItemView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooMuchToDoXamarinApp.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TooMuchToDoXamarinApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItemView : ContentPage
+	{
+		public ItemView(ItemViewModel viewModel)
+		{
+			InitializeComponent();
+			viewModel.Navigation = Navigation;
+			BindingContext = viewModel;
+		}
+	}
+}
