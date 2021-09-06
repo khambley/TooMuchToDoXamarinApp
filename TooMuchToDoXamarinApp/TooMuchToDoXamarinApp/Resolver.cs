0bda2c4725f38c552fa07e6ca8b03de1b3e9dcef
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 23:12:32 -0500

    Added Autofac and Resolver class

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Resolver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace TooMuchToDoXamarinApp
+{
+	//The resolver is responsible for creating objects based on the type requested. pg 70
+	public class Resolver
+	{
+		private static IContainer container;
+
+		public static void Initialize(IContainer container)
+		{
+			Resolver.container = container;
+		}
+
+		public static T Resolve<T>()
+		{
+			return container.Resolve<T>();
+		}
+	}
+}
