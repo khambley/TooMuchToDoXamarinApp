0d825a1da883a8af4e104ecaafce22c7287e0851
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 22:58:44 -0500

    Added MainView and wired up MainViewModel in code-behind pg.66

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Views/MainView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TooMuchToDoXamarinApp.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TooMuchToDoXamarinApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainView : ContentPage
+	{
+		public MainView(MainViewModel viewModel)
+		{
+			InitializeComponent();
+			viewModel.Navigation = Navigation;
+
+			//This tells the Xamarin.Forms binding engine to use our
+			//ViewModel object for the bindings.
+			BindingContext = viewModel;
+		}
+	}
+}
