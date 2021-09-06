5f473dd0c64931f9c19948093df8b45fddba0cb5
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 22:44:56 -0500

    Added ItemViewModel class pg 63

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/ItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TooMuchToDoXamarinApp.Repositories;
+
+namespace TooMuchToDoXamarinApp.ViewModels
+{
+	//This represents the list of to-do items.
+	public class ItemViewModel : ViewModel
+	{
+		private readonly TodoItemRepository repository;
+
+		public ItemViewModel(TodoItemRepository repository)
+		{
+			this.repository = repository;
+		}
+	}
+}
