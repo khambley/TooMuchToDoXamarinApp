5979d8489854227e1e31bffebe9a4e765caabbde
Katherine Hambley <superdesigngirl@mac.com>
2021-09-05 22:11:04 -0500

    Added SQLite and set up db connection in repo

--- TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Models/TodoItem.cs
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Models/TodoItem.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace TooMuchToDoXamarinApp.Models
 {
 	public class TodoItem
 	{
+		[PrimaryKey, AutoIncrement]
 		public int Id { get; set; }
 		public string Title { get; set; }
 		public bool Completed { get; set; }
--- TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Repositories/TodoItemRepository.cs
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Repositories/TodoItemRepository.cs
@@ -4,11 +4,14 @@ using System.Text;
 using TooMuchToDoXamarinApp.Models;
 using System.IO;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace TooMuchToDoXamarinApp.Repositories
 {
 	public class TodoItemRepository : ITodoItemRepository
 	{
+		private SQLiteAsyncConnection connection;
+
 		public event EventHandler<TodoItem> OnItemAdded;
 		public event EventHandler<TodoItem> OnItemUpdated;
 
@@ -35,6 +38,28 @@ namespace TooMuchToDoXamarinApp.Repositories
 			else
 			{
 				await UpdateItem(item);
+			}
+		}
+		private async Task CreateConnection()
+		{
+			if(connection != null)
+			{
+				return;
+			}
+			var documentPath = Environment.GetFolderPath(
+								Environment.SpecialFolder.MyDocuments);
+			var databasePath = Path.Combine(documentPath, "TodoItems.db");
+
+			connection = new SQLiteAsyncConnection(databasePath);
+			await connection.CreateTableAsync<TodoItem>();
+
+			if(await connection.Table<TodoItem>().CountAsync() == 0)
+			{
+				await connection.InsertAsync(new TodoItem()
+				{
+					Title = "Welcome to TooMuchToDo",
+					DueDate = DateTime.Now
+				});
 			}
 		}
 
