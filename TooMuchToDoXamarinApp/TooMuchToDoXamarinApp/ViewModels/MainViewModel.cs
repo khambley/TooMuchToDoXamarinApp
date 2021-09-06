f87a1662a26f4b3d1a6ddf3e25d734050636d63b
Katherine Hambley <superdesigngirl@mac.com>
2021-09-06 17:10:15 -0500

    Added AddItem data binding to MainViewModel and View pg76-78

--- TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/MainViewModel.cs
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/MainViewModel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using TooMuchToDoXamarinApp.Repositories;
+using System.Windows.Input;
+using TooMuchToDoXamarinApp.Views;
+using Xamarin.Forms;
 
 namespace TooMuchToDoXamarinApp.ViewModels
 {
@@ -16,9 +19,19 @@ namespace TooMuchToDoXamarinApp.ViewModels
 			Task.Run(async () => await LoadData());
 		}
 
-		private async Task LoadData()
+		// All commands should be exposed as a generic ICommand type,
+		// and let the Resolver resolve the instance.
+		public ICommand AddItem => new Command(async () =>
 		{
+			//This line creates a new ItemView view through Resolver, and Autofac
+			//builds the necessary dependencies.
+			var itemView = Resolver.Resolve<ItemView>();
+			await Navigation.PushAsync(itemView);
+		});
 
-		}
+		private async Task LoadData() { }
+
+		
+
 	}
 }
