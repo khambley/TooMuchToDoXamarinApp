873c6652e65c19d7f32e160ed0bf173775e26ac1
Katherine Hambley <superdesigngirl@mac.com>
2021-09-06 22:42:04 -0500

    Added ValueConverter custom converter for bool value = color pg.85

---
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/Converters/StatusColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TooMuchToDoXamarinApp.Converters
+{
+	public class StatusColorConverter : IValueConverter
+	{
+		//pg.84 Colors fetched from resource file in App.xaml
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return (bool)value ?
+				(Color)Application.Current.Resources["CompletedColor"] :
+				(Color)Application.Current.Resources["ActiveColor"];
+		}
+		public object ConvertBack(object value, Type targetType, object paramenter, CultureInfo culture)
+		{
+			return null,
+		}
+	}
+}
--- TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/MainViewModel.cs
+++ TooMuchToDoXamarinApp/TooMuchToDoXamarinApp/ViewModels/MainViewModel.cs
@@ -6,6 +6,9 @@ using TooMuchToDoXamarinApp.Repositories;
 using System.Windows.Input;
 using TooMuchToDoXamarinApp.Views;
 using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TooMuchToDoXamarinApp.Models;
 
 namespace TooMuchToDoXamarinApp.ViewModels
 {
@@ -13,8 +16,19 @@ namespace TooMuchToDoXamarinApp.ViewModels
 	{
 		private readonly TodoItemRepository repository;
 
+		//This collection can notify listeners about changes in the list, when 
+		//items are added or deleted. Used with Listview control, listems to changes
+		//in the list and updates itself automatically. pg.80
+		public ObservableCollection<TodoItemViewModel> Items { get; set; }
 		public MainViewModel(TodoItemRepository repository)
 		{
+			//If item is added to repository, Mainview will add it to the items list.
+			//The list updates via the ObservableCollection. pg. 82
+			repository.OnItemAdded += (sender, item) => Items.Add(CreateTodoItemViewModel(item));
+
+			//If an item is updated, the list simply reloads.
+			repository.OnItemUpdated += (sender, item) => Task.Run(async () => await LoadData());
+
 			this.repository = repository;
 			Task.Run(async () => await LoadData());
 		}
@@ -28,8 +42,30 @@ namespace TooMuchToDoXamarinApp.ViewModels
 			var itemView = Resolver.Resolve<ItemView>();
 			await Navigation.PushAsync(itemView);
 		});
+		
+		//The LoadData method calls the repository to fetch all item.
+		private async Task LoadData()
+		{
+			var items = await repository.GetItems();
 
-		private async Task LoadData() { }
+			//Wraps each to-do list item in TodoItemViewModel
+			//(use ViewModel and not Model, best practice pg.81)
+			var itemViewModels = items.Select(i => CreateTodoItemViewModel(i));
+
+			Items = new ObservableCollection<TodoItemViewModel>(itemViewModels);
+		}
+
+		private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+		{
+			var itemViewModel = new TodoItemViewModel(item);
+			itemViewModel.ItemStatusChanged += ItemStatusChanged;
+			return itemViewModel;
+		}
+
+		private void ItemStatusChanged(object sender, EventArgs e)
+		{
+
+		}
 
 		
 
